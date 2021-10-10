465f597826b8b3507d0f2afd9b6bea01db902025
ElvisRauza <elvis.rauza@gmail.com>
2021-10-10 17:28:47 +0300

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: 'ElvisRauza',
       exec_mode: 'cluster',
+      port: 3000,
       instances: 'max',
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start',
